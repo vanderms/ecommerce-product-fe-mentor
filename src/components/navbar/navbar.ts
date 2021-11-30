8784963c864f6d7406a6b7f9b87fd91d186b0a11
vanderms <vanderms.84@gmail.com>
2021-11-30 06:57:15 -0300

    create product section

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -30,10 +30,10 @@ class Navbar {
 
     const temp = document.createElement('template') as HTMLTemplateElement;
     temp.innerHTML = /*html*/`    
-      <button class='btn-close'>
+      <button aria-label='close menu' class='btn-close'>
         <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd"/></svg>
       </button>
-    `;    
+    `;
     this.sidebar.insertBefore(temp.content, this.sidebar.firstChild);
     return this.sidebar.querySelector('.btn-close') as HTMLElement;
   }
@@ -42,7 +42,7 @@ class Navbar {
   private createMenuButton(){
     const temp = document.createElement('template') as HTMLTemplateElement;
     temp.innerHTML = /*html*/`    
-    <button class='menu-btn close'> 
+    <button aria-label='menu' class='menu-btn close'> 
     <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z" fill="#69707D" fill-rule="evenodd"/></svg>
     </button>
     `;    
